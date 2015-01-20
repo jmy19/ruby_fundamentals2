24fb93db1b16b4b021b451bdadf3c08d1bbb1558
jmy19 <jasonmyers@rogers.com>
2015-01-20 17:33:34 -0500

    added banana reminder

--- exercise6.rb
+++ exercise6.rb
@@ -9,3 +9,9 @@ grocery_list.push("rice")
 listprint(grocery_list)
 
 puts "total items on the list: #{grocery_list.count}"
+
+if grocery_list.include?("Bananas")
+	puts "You need to pick up Bananas"
+else
+	puts "You do not need to pick up bananas"
+end
