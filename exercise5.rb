56f05b10e20b767ad6a8173b123850946d93e787
jmy19 <jasonmyers@rogers.com>
2015-01-20 16:52:11 -0500

    added method and output results

--- exercise5.rb
+++ exercise5.rb
@@ -1,5 +1,8 @@
 puts "What is the temperature in Fahrenheit?"
 f = gets.chomp.to_i
-
+def f_to_c(x)
+	c = (x - 32) * (5.0 / 9)
+end
+puts "#{f} degrees Farenheit converts to #{f_to_c(f)} degrees Celcius"
 
 
