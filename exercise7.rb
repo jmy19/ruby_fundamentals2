01c10db0755d9ba0840578d5dd9c502aa0fc65bb
jmy19 <jasonmyers@rogers.com>
2015-01-21 11:20:21 -0500

    UPDATED FINAL VERSION - added comments and cleaned up formatting

--- exercise7.rb
+++ exercise7.rb
@@ -1,3 +1,4 @@
+# step 1
 students = {
   cohort1: 34,
   cohort2: 42,
@@ -12,19 +13,23 @@ end
 
 listprint(students)
 
+#step 2
 students[:cohort4] = 43
 
+#step 3
 puts students.keys
 
+#step 4
 students.each_value do |y|
 	new_v = y *= 1.05
 	puts new_v.to_i
 end
 
+#step 5
 students.delete(:cohort2)
-
 listprint(students)
 
+#step 6
 total_students = 0
 
 students.each_value do |y|
